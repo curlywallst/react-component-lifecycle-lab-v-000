676c4ce32c3ea54437fad2cfb5f3bc2c0404c13b
curlywallst <nnoyes@comcast.net>
2017-10-24 11:10:57 -0400

    Done.

--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -10,9 +10,20 @@ class TweetWall extends React.Component {
     };
   }
 
-  // TODO: componentWillMount()
-  // TODO: shouldComponentUpdate()
-  // TODO: componentWillReceiveProps()
+  componentWillMount() {
+    this.setState({tweets: this.props.newTweets})
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (this.props.newTweets.length > 0)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.newTweets.length > 0) {
+      const allTweets = nextProps.newTweets.concat(this.state.tweets)
+      this.setState({tweets: allTweets})
+    }
+  }
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
